41493ba1776d48ddf93c4acd1a3fb24536eda92a
E7380 <bboubacar366@gmail.com>
2023-11-03 20:36:41 +0100

    feat : axios url

--- app/config/mongodb.config.js
+++ app/config/mongodb.config.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://bboubacarbarry2001:o5wIorAkvbKrw8ua@eduboubacluster.sq65m4b.mongodb.net/plant_shooping_db",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("connection reussie !"))
   .catch((err) => {
     console.log("error connecting : " + err);
